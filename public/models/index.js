0c378e583102fce7b8226b16ca4ee2b89b276b5e
Illio, the Great <pwfcoding@gmail.com>
2019-12-05 19:54:18 -0600

    some directory changes

--- public/models/index.js
+++ public/models/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from '../../src/App';
+import * as serviceWorker from '../../src/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
+module.exports = {
+    User: require("./user"),
+    Review: require("./review")
+};
+
 serviceWorker.unregister();
