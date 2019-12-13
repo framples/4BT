bf9aa2cce52d4aa04652b30a373defeebbb2cdef
PeterFairley <pwfcoding@gmail.com>
2019-12-13 14:43:42 -0600

    somewhat working auth and directory structure

--- server.js
+++ server.js
@@ -1,58 +1,29 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoStore = require('connect-mongo')(session);
+let express = require("express");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+let app = express();
+let mongoose = require("mongoose");
+let port = process.env.PORT || 5000
 
-//connect to MongoDB
-mongoose.connect('mongodb://localhost/testForAuth');
-var db = mongoose.connection;
-
-//handle mongo error
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-});
-
-//use sessions for tracking logins
-app.use(session({
-  secret: 'work hard',
-  resave: true,
-  saveUninitialized: false,
-  store: new MongoStore({
-    mongooseConnection: db
+app.use(bodyParser.json())
+app.use(cors())
+app.use(
+  bodyParser.urlencoded({
+    extended: false
   })
-}));
+)
 
-// parse incoming requests
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const mongoURI = 'mongodb://localhost:27017/mernloginreg'
 
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected!"))
+  .catch(err => console.log(err))
 
-// serve static files from template
-app.use(express.static(__dirname + '/public/templateLogReg'));
+let Users = require('./routes/Users')
 
-// include routes
-var routes = require('./public/routes/router');
-app.use('/', routes);
+app.use('/users', Users);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('File Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-// define as the last app.use callback
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.send(err.message);
-});
-
-
-// listen on port 3000
-app.listen(3000, function () {
-  console.log('Express app listening on port 3000');
+app.listen(port, () => {
+  console.log("Server is running on port: " + port)
 });
