eb017ffd758507b915323d3201e1aaa117d36b53
Illio, the Great <pwfcoding@gmail.com>
2019-12-14 11:56:34 -0600

    logout function working and profile error fixed.

--- routes/Users.js
+++ routes/Users.js
@@ -9,7 +9,7 @@ users.use(cors())
 
 process.env.SECRET_KEY = 'secret'
 
-users.post('/register', (req,res) => {
+users.post('/register', (req, res) => {
     const today = new Date()
     const userData = {
         first_name: req.body.first_name,
@@ -22,54 +22,54 @@ users.post('/register', (req,res) => {
     User.findOne({
         email: req.body.email
     })
-    .then(user => {
-        if(!user) {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                userData.password = hash
-                User.create(userData)
-                .then(user => {
-                    res.json({status: user.email + ' registered!'})
+        .then(user => {
+            if (!user) {
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    userData.password = hash
+                    User.create(userData)
+                        .then(user => {
+                            res.json({ status: user.email + ' registered!' })
+                        })
+                        .catch(err => {
+                            res.send('error: ' + err)
+                        })
                 })
-                .catch(err => {
-                    res.send('error: ' + err)
-                })
-            })
-        } else {
-            res.json({error: 'User already exists'})
-        }
-    })
-    .catch(err => {
-        res.send('error: ' + err)
-    })
+            } else {
+                res.json({ error: 'User already exists' })
+            }
+        })
+        .catch(err => {
+            res.send('error: ' + err)
+        })
 })
 
 users.post('/login', (req, res) => {
     User.findOne({
         email: req.body.email
     })
-    .then(user => {
-        if(user) {
-            if(bcrypt.compareSync(req.body.password, user.password)) {
-                const payload = {
-                    _id: user._id,
-                    first_name: user.first_name,
-                    last_name: user.last_name,
-                    email: user.email
+        .then(user => {
+            if (user) {
+                if (bcrypt.compareSync(req.body.password, user.password)) {
+                    const payload = {
+                        _id: user._id,
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        email: user.email
+                    }
+                    let token = jwt.sign(payload, process.env.SECRET_KEY, {
+                        expiresIn: 1440
+                    })
+                    res.send(token)
+                } else {
+                    res.status(404).send({ error: "User does not exist" })
                 }
-                let token = jwt.sign(payload, process.env.SECRET_KEY, {
-                    expiresIn: 1440
-                })
-                res.send(token)
             } else {
-                res.json({error: "User does not exist"})
+                res.status(404).send({ error: "User does not exist" })
             }
-        }else {
-            res.json({error: "User does not exist"})
-        }
-    })
-    .catch(err => {
-        res.send('error: ' + err) 
-    })
+        })
+        .catch(err => {
+            res.send('error: ' + err)
+        })
 });
 
 users.get('/profile', (req, res) => {
@@ -78,16 +78,16 @@ users.get('/profile', (req, res) => {
     User.findOne({
         _id: decoded._id
     })
-    .then(user => {
-        if(user) {
-            res.json(user)
-        } else {
-            res.send("User does not exist")
-        }
-    })
-    .catch(err => {
-        res.send('error: ' + err)
-    })
+        .then(user => {
+            if (user) {
+                res.json(user)
+            } else {
+                res.send("User does not exist")
+            }
+        })
+        .catch(err => {
+            res.send('error: ' + err)
+        })
 })
 
 
